501a37635b6f262f3e3dd3f0e7edabf7d03c773e
mikel-x1 <mikel-x1@yandex.ru>
2023-02-11 13:56:21 +0100

    linter fix

--- brain_games/Engine.py
+++ brain_games/Engine.py
@@ -15,7 +15,10 @@ def game(rule, function):
             print('Correct!')
             i += 1
         else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {right_answer}.")
+            print(
+                f"{answer} is wrong answer ;(. "
+                f"Correct answer was {right_answer}."
+            )
             print(f"Let's try again, {name}!")
             break
     if i == 3:
